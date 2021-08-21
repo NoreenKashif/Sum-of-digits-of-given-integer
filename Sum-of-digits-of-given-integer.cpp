3543b37cd4a7ac6eeca42860ce58b478999518d7
NoreenKashif <89309542+NoreenKashif@users.noreply.github.com>
2021-08-21 21:49:50 +0500

    Add files via upload
    The program will return the sum of digits of an integer that the user will enter.

---
+++ Sum-of-digits-of-given-integer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num,sum=0,temp, num1;
+	cout << "Enter an integer: ";
+	cin >> num;
+	num1 = num;
+	while (num != 0)
+	{
+		temp = num % 10;
+		sum = sum + temp;
+		num = num / 10;
+	}
+	cout << endl;
+	cout << "Sum of digits of a " << num1 << " is : " << sum;
+
+	return 0;
+}
