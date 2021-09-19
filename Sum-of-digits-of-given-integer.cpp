83be4f635fcd74ec58bca114df63526faeb30e7e
NoreenKashif <89309542+NoreenKashif@users.noreply.github.com>
2021-09-19 18:01:46 +0500

    Update Sum-of-digits-of-given-integer.cpp

--- Sum-of-digits-of-given-integer.cpp
+++ Sum-of-digits-of-given-integer.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main()
 {
 	int num,sum=0,temp, num1;
-	cout << "Enter an integer: ";
+	cout << "Enter an integer: ";//getting an integer
 	cin >> num;
 	num1 = num;
 	while (num != 0)
 	{
-		temp = num % 10;
+		temp = num % 10;//getting the right most digit
 		sum = sum + temp;
-		num = num / 10;
+		num = num / 10;//updating the number
 	}
 	cout << endl;
 	cout << "Sum of digits of a " << num1 << " is : " << sum;
